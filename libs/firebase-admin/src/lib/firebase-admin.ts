fba4af4222d8947c254f77c5c09731d3ecb3ea09
TavaresDylan <tavares.dylan03@gmail.com>
2024-04-22 20:59:42 +0200

    chore: firebase admin lib added

---
+++ libs/firebase-admin/src/lib/firebase-admin.ts
@@ -0,0 +1,23 @@
+import * as firebase from 'firebase-admin';
+
+const firebaseParams = {
+  type: process.env['FIREBASE_TYPE'],
+  project_id: process.env['FIREBASE_PROJECT_ID'],
+  private_key_id: process.env['FIREBASE_PRIVATE_KEY_ID'],
+  private_key: process.env['FIREBASE_PRIVATE_KEY'],
+  client_email: process.env['FIREBASE_CLIENT_EMAIL'],
+  client_id: process.env['FIREBASE_CLIENT_ID'],
+  auth_uri: process.env['FIREBASE_AUTH_URI'],
+  token_uri: process.env['FIREBASE_TOKEN_URI'],
+  auth_provider_x509_cert_url:
+    process.env['FIREBASE_AUTH_PROVIDER_X509_CERT_URL'],
+  client_x509_cert_url: process.env['FIREBASE_CLIENT_X509_CERT_URL'],
+  universe_domain: process.env['FIREBASE_UNIVERSE_DOMAIN'],
+};
+
+export const FirebaseAdminApp = firebase.initializeApp({
+  credential: firebase.credential.cert(
+    firebaseParams as firebase.ServiceAccount
+  ),
+  databaseURL: 'https://argolightspace.firebaseio.com',
+});
