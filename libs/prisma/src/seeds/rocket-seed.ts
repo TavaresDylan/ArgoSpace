ab307e19de4c4e1ab2742c2d573d0a2c0071293b
TavaresDylan <tavares.dylan03@gmail.com>
2024-04-29 10:31:34 +0200

    refactor: seed mode changed as "upsert"

--- libs/prisma/src/seeds/rocket-seed.ts
+++ libs/prisma/src/seeds/rocket-seed.ts
@@ -1,8 +1,13 @@
 import { PrismaClient } from '@prisma/client';
 
 export async function RocketSeed(prisma: PrismaClient) {
-  await prisma.rocket.create({
-    data: {
+  await prisma.rocket.upsert({
+    where: {
+      id: '1',
+    },
+    update: {},
+    create: {
+      id: '1',
       name: 'Falcon 1',
       description:
         'The Falcon 1 was an expendable launch system privately developed and manufactured by SpaceX during 2006-2009. On 28 September 2008, Falcon 1 became the first privately-developed liquid-fuel launch vehicle to go into orbit around the Earth.',
@@ -25,8 +30,13 @@ export async function RocketSeed(prisma: PrismaClient) {
       country: 'Republic of the Marshall Islands',
     },
   });
-  await prisma.rocket.create({
-    data: {
+  await prisma.rocket.upsert({
+    where: {
+      id: '2',
+    },
+    update: {},
+    create: {
+      id: '2',
       name: 'Falcon 9',
       description:
         'Falcon 9 is a two-stage rocket designed and manufactured by SpaceX for the reliable and safe transport of satellites and the Dragon spacecraft into orbit.',
@@ -64,8 +74,13 @@ export async function RocketSeed(prisma: PrismaClient) {
       totalReflights: 255,
     },
   });
-  await prisma.rocket.create({
-    data: {
+  await prisma.rocket.upsert({
+    where: {
+      id: '3',
+    },
+    update: {},
+    create: {
+      id: '3',
       name: 'Falcon Heavy',
       description:
         'With the ability to lift into orbit over 54 metric tons (119,000 lb)--a mass equivalent to a 737 jetliner loaded with passengers, crew, luggage and fuel--Falcon Heavy can lift more than twice the payload of the next closest operational vehicle, the Delta IV Heavy, at one-third the cost.',
@@ -97,8 +112,13 @@ export async function RocketSeed(prisma: PrismaClient) {
       totalReflights: 14,
     },
   });
-  await prisma.rocket.create({
-    data: {
+  await prisma.rocket.upsert({
+    where: {
+      id: '4',
+    },
+    update: {},
+    create: {
+      id: '4',
       name: 'Starship',
       description:
         'Starship and Super Heavy Rocket represent a fully reusable transportation system designed to service all Earth orbit needs as well as the Moon and Mars. This two-stage vehicle — composed of the Super Heavy rocket (booster) and Starship (ship) — will eventually replace Falcon 9, Falcon Heavy and Dragon.',
